2a2a8fe7691bfa19ca065a3737f134288105502d
nmso2 <nmsubho@gmail.com>
2022-05-08 15:22:05 +0600

    added basic card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchLaunches } from "./redux/slices/launchesSlice";
 import { Button } from "react-bootstrap";
+import LaunchesCard from "./components/LaunchesCard/LaunchesCard";
 
 function App() {
   const launches = useSelector((state) => state.launches.launches);
@@ -17,9 +18,20 @@ function App() {
   return (
     <div className="App">
       <Button>Button</Button>
-      {launches.map((launch) => (
-        <p>{launch.mission_name}</p>
-      ))}
+      <section class="bg-light pt-5 pb-5 shadow-sm">
+        <div class="container">
+          <div class="row pt-5">
+            <div class="col-12">
+              <h3 class="text-uppercase border-bottom mb-4">Launches</h3>
+            </div>
+          </div>
+          <div class="row">
+            {launches.map((launch) => (
+              <LaunchesCard launch={launch} />
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/LaunchesCard/LaunchesCard.js
+++ src/components/LaunchesCard/LaunchesCard.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const LaunchesCard = () => {
+const LaunchesCard = ({ launch }) => {
   return (
-    <div>
-      <p>Card</p>
+    <div class="col-lg-4 mb-3 d-flex align-items-stretch">
+      <div class="card">
+        <img src={launch.links.mission_patch} class="card-img-top" alt="Card" />
+        <div class="card-body d-flex flex-column">
+          <h5 class="card-title">{launch.mission_name}</h5>
+          <p class="card-text mb-4">{launch.details}</p>
+          <a
+            href="/"
+            class="btn btn-primary text-white mt-auto align-self-start"
+          >
+            Book now
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
