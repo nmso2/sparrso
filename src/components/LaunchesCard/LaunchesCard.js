9a91580412982f90bee1cf5d7ae5fadc2cc2dae9
nmso2 <nmsubho@gmail.com>
2022-05-09 18:41:19 +0600

    card design

--- src/components/LaunchesCard/LaunchesCard.js
+++ src/components/LaunchesCard/LaunchesCard.js
@@ -1,49 +1,58 @@
-import moment from "moment";
 import React from "react";
+import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faRocket } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap";
 
 const LaunchesCard = ({ launch }) => {
-  // function filterDate() {
-  //   var today = new Date();
-  //   var filterDate = new Date(
-  //     today.getFullYear() - 3,
-  //     today.getMonth(),
-  //     today.getDate()
-  //   );
-  //   return filterDate;
-  // }
-  // var today = moment(launch.launch_date_local).format();
-  // console.log(filterDate() < moment(launch.launch_date_local).year());
-  // console.log(filterDate(), "::::", moment(launch.launch_date_local).format());
-  // console.log(today);
-  // console.log("filterDate::", filterDate());
-  // console.log("launchdate::", new Date(launch.launch_date_local));
-  // console.log(filterDate() < new Date(launch.launch_date_local));
-
   return (
     <div className="col-lg-4 mb-3 d-flex align-items-stretch">
-      <div className="card">
-        <img
-          src={launch.links.mission_patch}
-          className="card-img-top"
-          alt="Card"
-        />
+      <div className="card shadow-lg">
+        <div className="text-center">
+          <img
+            src={launch.links.mission_patch}
+            className="card-img-top p-3 w-75"
+            alt="Card"
+          />
+        </div>
         <div className="card-body d-flex flex-column">
           <h5 className="card-title">{launch.mission_name}</h5>
           <p className="card-text mb-4">
             {moment(launch.launch_date_local, "YYYYMMDD").fromNow()} on{" "}
-            {moment(launch.launch_date_local).format("MMMM Do YYYY, h:mm:ss a")}
+            {moment(launch.launch_date_local).format("MMMM Do YYYY")}
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faLocationDot} />{" "}
+            {launch.launch_site.site_name}
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faRocket} /> {launch.rocket.rocket_name}
           </p>
           {/* <p className="card-text mb-4">
             {moment(launch.launch_date_local).subtract(6, "days").calendar()}
           </p> */}
-          <p className="card-text mb-4">{launch.details}</p>
+          {/* <p className="card-text mb-4">{launch.details}</p> */}
 
-          <a
-            href="/"
-            className="btn btn-primary text-white mt-auto align-self-start"
-          >
-            Book now
-          </a>
+          <div className=" d-flex text-center align-content-center justify-content-around">
+            <a
+              href={launch.links.article_link}
+              className="btn btn-outline-dark mt-auto"
+            >
+              Details
+            </a>
+            <a
+              href={launch.links.video_link}
+              className="btn btn-outline-dark mt-auto"
+            >
+              Video
+            </a>
+            <a
+              href={launch.links.wikipedia}
+              className="btn btn-outline-dark mt-auto"
+            >
+              Wikipedia
+            </a>
+          </div>
         </div>
       </div>
     </div>
