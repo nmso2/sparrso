e1e18498382f78c06045bbdb0bc1b4ae339534c9
nmso2 <nmsubho@gmail.com>
2022-05-10 00:22:19 +0600

    filter option updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,23 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchLaunches } from "../../redux/slices/launchesSlice";
 import LaunchesCard from "../LaunchesCard/LaunchesCard";
-import {
-  Button,
-  Dropdown,
-  DropdownButton,
-  FormControl,
-  InputGroup,
-} from "react-bootstrap";
+import { Button, Dropdown, DropdownButton } from "react-bootstrap";
 
 const Home = () => {
   const [searchText, setSearchText] = useState("");
   const [displayData, setDisplayData] = useState([]);
-  const [filterState, setFilterState] = useState(true);
-  const [monthSelected, setMonthSelected] = useState(false);
-  const [yearSelected, setYearSelected] = useState(false);
-  const [weekSelected, setWeekSelected] = useState(false);
-  const [upcommingSelected, setUpcommingSelected] = useState(false);
   const [isSuccess, setIsSuccess] = useState("");
+  const [duration, setDuration] = useState("");
+  const [upcomming, setUpcomming] = useState("");
 
   const launches = useSelector((state) => state.launches.launches);
   const dispatch = useDispatch();
@@ -37,53 +28,14 @@ const Home = () => {
   d1.setDate(0); // going to 1st of the month
 
   const clearFilter = () => {
-    setMonthSelected(false);
-    setYearSelected(false);
-    setWeekSelected(false);
-    setUpcommingSelected(false);
     setIsSuccess("");
+    setDuration("");
+    setUpcomming("");
+    setSearchText("");
   };
 
   useEffect(() => {
-    yearSelected
-      ? setDisplayData(
-          launches.filter(
-            (search) =>
-              search.rocket.rocket_name
-                .toLowerCase()
-                .includes(searchText.toLowerCase()) &&
-              y < new Date(search.launch_date_local) &&
-              new Date(search.launch_date_local) <
-                new Date(y.getFullYear(), 11, 31)
-          )
-        )
-      : monthSelected
-      ? setDisplayData(
-          launches.filter(
-            (search) =>
-              search.rocket.rocket_name
-                .toLowerCase()
-                .includes(searchText.toLowerCase()) &&
-              m < new Date(search.launch_date_local) &&
-              new Date(search.launch_date_local) < d1
-          )
-        )
-      : weekSelected
-      ? setDisplayData(
-          launches.filter(
-            (search) =>
-              search.rocket.rocket_name
-                .toLowerCase()
-                .includes(searchText.toLowerCase()) &&
-              new Date(
-                today.getFullYear(),
-                today.getMonth(),
-                today.getDate() - 14
-              ) < new Date(search.launch_date_local) &&
-              new Date(search.launch_date_local) < w
-          )
-        )
-      : upcommingSelected
+    upcomming === "upcomming"
       ? setDisplayData(
           launches.filter(
             (search) =>
@@ -110,6 +62,44 @@ const Home = () => {
                 .includes(searchText.toLowerCase()) && !search.launch_success
           )
         )
+      : duration === "last_week"
+      ? setDisplayData(
+          launches.filter(
+            (search) =>
+              search.rocket.rocket_name
+                .toLowerCase()
+                .includes(searchText.toLowerCase()) &&
+              new Date(
+                today.getFullYear(),
+                today.getMonth(),
+                today.getDate() - 14
+              ) < new Date(search.launch_date_local) &&
+              new Date(search.launch_date_local) < w
+          )
+        )
+      : duration === "last_month"
+      ? setDisplayData(
+          launches.filter(
+            (search) =>
+              search.rocket.rocket_name
+                .toLowerCase()
+                .includes(searchText.toLowerCase()) &&
+              m < new Date(search.launch_date_local) &&
+              new Date(search.launch_date_local) < d1
+          )
+        )
+      : duration === "last_year"
+      ? setDisplayData(
+          launches.filter(
+            (search) =>
+              search.rocket.rocket_name
+                .toLowerCase()
+                .includes(searchText.toLowerCase()) &&
+              y < new Date(search.launch_date_local) &&
+              new Date(search.launch_date_local) <
+                new Date(y.getFullYear(), 11, 31)
+          )
+        )
       : setDisplayData(
           launches.filter((search) =>
             search.rocket.rocket_name
@@ -118,15 +108,7 @@ const Home = () => {
           )
         );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    launches,
-    monthSelected,
-    searchText,
-    yearSelected,
-    weekSelected,
-    upcommingSelected,
-    isSuccess,
-  ]);
+  }, [launches, searchText, isSuccess, duration, upcomming]);
 
   console.log("displayData::", displayData);
 
@@ -134,108 +116,115 @@ const Home = () => {
     <section className=" bg-light pt-5 pb-5 shadow-sm">
       <div className="container">
         <div>
-          <p>Search</p>
-          <div>
-            <InputGroup className="mb-3 " size="lg">
-              <FormControl
-                className="display-6"
-                placeholder="Search by Rocket Name..."
-                aria-label="Search by Rocket Name..."
-                aria-describedby="basic-addon1"
-                onChange={(e) => setSearchText(e.target.value)}
-                value={searchText}
-              />{" "}
-              <Button
-                className=" ms-2"
-                onClick={() => setFilterState(!filterState)}
-              >
-                Filter
-              </Button>
-            </InputGroup>
+          <div className="d-md-flex justify-content-center">
+            <input
+              type="text"
+              className="form-control-sm border-1"
+              placeholder="Search by Rocket Name..."
+              aria-label="Search by Rocket Name..."
+              onChange={(e) => setSearchText(e.target.value)}
+              value={searchText}
+            />
 
-            <div hidden={filterState}>
-              <div className="ms-3 d-md-inline">
-                <input
-                  type="checkbox"
-                  name="week"
-                  id="week"
-                  checked={weekSelected}
-                  onChange={(e) => setWeekSelected(e.target.checked)}
-                />
-                <label className="ms-1" htmlFor="week">
-                  Last Week
-                </label>
-              </div>
-              <div className="ms-3 d-md-inline">
-                <input
-                  type="checkbox"
-                  name="month"
-                  id="month"
-                  checked={monthSelected}
-                  onChange={(e) => setMonthSelected(e.target.checked)}
-                />
-                <label className="ms-1" htmlFor="month">
-                  Last Month
-                </label>
-              </div>
-              <div className="ms-3 d-md-inline">
-                <input
-                  type="checkbox"
-                  name="year"
-                  id="year"
-                  checked={yearSelected}
-                  onChange={(e) => setYearSelected(e.target.checked)}
-                />
-                <label className="ms-1" htmlFor="year">
-                  Last Year
-                </label>
-              </div>
-              <div className="ms-3 d-md-inline">
-                <input
-                  type="checkbox"
-                  name="upcomming"
-                  id="upcomming"
-                  checked={upcommingSelected}
-                  onChange={(e) => setUpcommingSelected(e.target.checked)}
-                />
-                <label className="ms-1" htmlFor="upcomming">
-                  Upcomming
-                </label>
-              </div>
+            <DropdownButton
+              variant="outline-dark"
+              className="ms-3"
+              id="dropdown-item-button"
+              title="Launch Date"
+            >
+              <Dropdown.Item
+                as="button"
+                value={"last_week"}
+                onClick={(e) => {
+                  setIsSuccess("");
+                  setUpcomming("");
+                  setDuration(e.target.value);
+                }}
+              >
+                Last Week
+              </Dropdown.Item>
+              <Dropdown.Item
+                as="button"
+                value={"last_month"}
+                onClick={(e) => {
+                  setIsSuccess("");
+                  setUpcomming("");
+                  setDuration(e.target.value);
+                }}
+              >
+                Last Month
+              </Dropdown.Item>
+              <Dropdown.Item
+                as="button"
+                value={"last_year"}
+                onClick={(e) => {
+                  setIsSuccess("");
+                  setUpcomming("");
+                  setDuration(e.target.value);
+                }}
+              >
+                Last Year
+              </Dropdown.Item>
+            </DropdownButton>
 
-              {/* ================================================= */}
-              <DropdownButton
-                variant="outline-dark"
-                className="ms-3 d-md-inline"
-                id="dropdown-item-button"
-                title="Launch Status"
+            <DropdownButton
+              variant="outline-dark"
+              className="ms-3"
+              id="dropdown-item-button"
+              title="Launch Status"
+            >
+              <Dropdown.Item
+                as="button"
+                value={"fail"}
+                onClick={(e) => {
+                  setDuration("");
+                  setUpcomming("");
+                  setIsSuccess(e.target.value);
+                }}
               >
-                <Dropdown.Item
-                  as="button"
-                  value={"fail"}
-                  onClick={(e) => setIsSuccess(e.target.value)}
-                >
-                  Failure
-                </Dropdown.Item>
-                <Dropdown.Item
-                  as="button"
-                  value={"success"}
-                  onClick={(e) => setIsSuccess(e.target.value)}
-                >
-                  Success
-                </Dropdown.Item>
-              </DropdownButton>
-              <Button
-                variant="outline-danger"
-                onClick={clearFilter}
-                className="ms-3 d-md-inline"
+                Failure
+              </Dropdown.Item>
+              <Dropdown.Item
+                as="button"
+                value={"success"}
+                onClick={(e) => {
+                  setDuration("");
+                  setUpcomming("");
+                  setIsSuccess(e.target.value);
+                }}
               >
-                Clear Filter
-              </Button>
-              {/* ================================================= */}
-            </div>
+                Success
+              </Dropdown.Item>
+            </DropdownButton>
+
+            <DropdownButton
+              variant="outline-dark"
+              className="ms-3"
+              id="dropdown-item-button"
+              title="Upcoming?"
+            >
+              <Dropdown.Item
+                as="button"
+                value={"upcomming"}
+                onClick={(e) => {
+                  setDuration("");
+                  setIsSuccess("");
+                  setUpcomming(e.target.value);
+                }}
+              >
+                Upcomming
+              </Dropdown.Item>
+            </DropdownButton>
+            <Button
+              variant="outline-danger"
+              onClick={clearFilter}
+              className="ms-3"
+            >
+              Clear Filter
+            </Button>
           </div>
         </div>
+        {/* </div> */}
         <div className="row pt-3">
           <div className="col-12">
             <h3 className="text-uppercase border-bottom pb-2 mb-4">Launches</h3>
