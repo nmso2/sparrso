a1bc4bcde2af8d0b7920e229e7f3f55fa28c20dc
nmso2 <nmsubho@gmail.com>
2022-05-10 11:08:20 +0600

    card design improved and added photo modal

--- src/components/LaunchesCard/LaunchesCard.js
+++ src/components/LaunchesCard/LaunchesCard.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faRocket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faInfoCircle,
+  faLocationDot,
+  faRocket,
+  faVideo,
+  faW,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  Button,
+  Carousel,
+  Modal,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 
 const LaunchesCard = ({ launch }) => {
+  const [show, setShow] = useState(false);
   return (
     <div className="col-lg-4 mb-3 d-flex align-items-stretch">
       <div className="card shadow-lg">
@@ -12,6 +26,8 @@ const LaunchesCard = ({ launch }) => {
             src={launch.links.mission_patch}
             className="card-img-top p-3 w-75"
             alt="Card"
+            onClick={() => setShow(true)}
+            role="button"
           />
         </div>
         <div className="card-body d-flex flex-column">
@@ -29,33 +45,118 @@ const LaunchesCard = ({ launch }) => {
           </p>
 
           <div className=" d-flex text-center align-content-center justify-content-around">
-            <a
-              href={launch.links.article_link}
-              className="btn btn-outline-dark mt-auto"
-              target="_blank"
-              rel="noreferrer"
+            <OverlayTrigger
+              overlay={<Tooltip id="tooltip-disabled">Mission Info</Tooltip>}
             >
-              Details
-            </a>
-            <a
-              href={launch.links.video_link}
-              className="btn btn-outline-dark mt-auto"
-              target="_blank"
-              rel="noreferrer"
+              <span className="d-inline-block">
+                <a
+                  href={launch.links.article_link}
+                  className="btn btn-outline-dark mt-auto"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon icon={faInfoCircle} />
+                </a>
+              </span>
+            </OverlayTrigger>
+
+            <OverlayTrigger
+              overlay={<Tooltip id="tooltip-disabled">Video</Tooltip>}
             >
-              Video
-            </a>
-            <a
-              href={launch.links.wikipedia}
-              className="btn btn-outline-dark mt-auto"
-              target="_blank"
-              rel="noreferrer"
+              <span className="d-inline-block">
+                <a
+                  href={launch.links.video_link}
+                  className="btn btn-outline-dark mt-auto"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon icon={faVideo} />
+                </a>
+              </span>
+            </OverlayTrigger>
+
+            <OverlayTrigger
+              overlay={<Tooltip id="tooltip-disabled">Wikipedia</Tooltip>}
             >
-              Wikipedia
-            </a>
+              <span className="d-inline-block">
+                <a
+                  href={launch.links.wikipedia}
+                  className="btn btn-outline-dark mt-auto"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon icon={faW} />
+                </a>
+              </span>
+            </OverlayTrigger>
           </div>
         </div>
       </div>
+      {/* =================Modal Start================== */}
+      <Modal show={show} onHide={() => setShow(false)} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>{launch.mission_name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="d-md-flex justify-content-around align-items-center">
+            <div>
+              <div className="mb-2">
+                {launch.launch_success ? (
+                  <p className="text-success my-0">
+                    This was a successfull mission!
+                  </p>
+                ) : (
+                  <p className="text-danger my-0">
+                    Unfortunately this mission was unsuccessfull!
+                  </p>
+                )}
+                {!launch.launch_success && (
+                  <p className="my-0 text-danger">
+                    Resson: {launch?.launch_failure_details?.reason}
+                  </p>
+                )}
+              </div>
+
+              <div className="mb-2">
+                {" "}
+                <p className="my-0">
+                  Launch Date:{" "}
+                  {moment(launch.launch_date_local).format("MMMM Do YYYY")}
+                </p>
+                <p className="my-0">Location: {launch.launch_site.site_name}</p>
+              </div>
+
+              <div className="mb-2">
+                <p className="my-0">Rocket Name: {launch.rocket.rocket_name}</p>
+                <p className="my-0">Rocket Type: {launch.rocket.rocket_type}</p>
+              </div>
+            </div>
+            <img src={launch.links.mission_patch_small} alt="Card" />
+          </div>
+          <div>
+            <p className="h4">Details</p>
+            <p>{launch.details}</p>
+            <Carousel>
+              {launch.links.flickr_images.length !== 0 &&
+                launch.links.flickr_images.map((imageLink) => (
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={imageLink}
+                      alt="slide"
+                    />
+                  </Carousel.Item>
+                ))}
+            </Carousel>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShow(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      {/* =================Modal End==================== */}
     </div>
   );
 };
